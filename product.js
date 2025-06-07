0f2dc2cda62792dc2e32df0813da5fb16f7389fe
noorafifi889 <noorelafifi889@gmail.com>
2025-06-07 12:21:00 +0300

    update and use a api

--- product.js
+++ product.js
@@ -3,14 +3,15 @@ console.log("Product ID is:", id);
 
 // جلب بيانات المنتج حسب ID
 const api = fetch(`https://fakestoreapi.com/products/${id}`)
-    // console.log(api)
+  // console.log(api)
   .then((res) => res.json())
-    .then((product) => {
-      console.log(product)
+  .then((product) => {
+    console.log(product);
     document.getElementById("product-title").textContent = product.title;
     document.getElementById("product-description").textContent =
       "Description: " + product.description;
-    document.getElementById("product-price").textContent = "Price: "+product.price + " $";
+    document.getElementById("product-price").textContent =
+      "Price: " + product.price + " $";
     document.getElementById("product-img").src = product.image;
   })
-  .catch((error) => console.error("حدث خطأ أثناء جلب بيانات المنتج:", error));
+  .catch((error) => console.error("Error when the data is fetching ", error));
--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-let cards = document.getElementsByClassName("card");
+const container = document.getElementById("products-container");
 
 fetch("https://fakestoreapi.com/products")
-  .then((response) => response.json())
+  .then((res) => res.json())
   .then((data) => {
-    for (let i = 0; i < data.length && i < cards.length; i++) {
-      const product = data[i];
-      cards[i].addEventListener("click", () => {
-        console.log(product);
-      });
-    }
+    data.forEach((product) => {
+      const card = document.createElement("div");
+      card.className = "col";
+      let title = product.title;
+      if (title.length > 50) {
+        title = title.substring(0, 40) + "...";
+      }
+      card.innerHTML = `
+        <a href="product.html?id=${product.id}" class="product" target="_blank" style="text-decoration: none; color: black;">
+          <div class="card h-100">
+            <img src="${product.image}" class="p-4 img-fluid d-flex justify-content-center" alt="${product.title}" style="object-fit: cover; height: 200px; margin-left:auto ; margin-right:auto">
+            <div class="card-body">
+              <h5 class="fs-6">${title}</h5>
+              <p class="card-text">${product.price} $</p>
+              <p class="text-black-50">Product Code: ${product.id}</p>
+              <div class="icon fs-4 d-flex justify-content-between align-items-center mr-5" style="margin-right: 16%">
+                <a href="#" class="icon-style"><i class="bi bi-cart-check icon-style"></i></a>
+                <a href="#" class="icon-style"><i class="bi bi-heart icon-style"></i></a>
+                <a href="#" class="icon-style"><i class="bi bi-share icon-style"></i></a>
+              </div>
+            </div>
+          </div>
+        </a>
+      `;
+      container.appendChild(card);
+    });
   })
-  .catch((error) => console.error("حدث خطأ أثناء جلب البيانات:", error));
-
-console.log("==================");
-// استخدم حلقة لإضافة المستمع لكل عنصر
+  .catch((err) => console.error("حدث خطأ أثناء جلب البيانات:", err));
